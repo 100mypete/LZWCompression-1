40c2b299b0871df56344417681e793dd12ac779d
Peter Shen <shenjiashu@hotmail.com>
2021-09-22 20:14:52 -0700

    rewrote decoder, but now everything is decoding to 0, so something is wrong

--- LZWCompression/src/Decoder.java
+++ LZWCompression/src/Decoder.java
@@ -14,42 +14,42 @@ public class Decoder {
 	
 	
 	public Decoder(String fileName) throws IOException {
-		sizeOfDictionary = 0;
-		//code below puts in normal chars into the table
-		dictionary = new HashMap<Integer, String> ();
-		for(int index = 0; index < 256; index++) {
-			char newChar = (char)index; //changes the index of ASCII to a character
-			String toBeAdded = "" + newChar; //changes the character to a string
-			dictionary.put(index, toBeAdded); //puts string into HashMap with corresponding spot in table
-			sizeOfDictionary++;
-		}
+		// sizeOfDictionary = 0;
+		// //code below puts in normal chars into the table
+		// dictionary = new HashMap<Integer, String> ();
+		// for(int index = 0; index < 256; index++) {
+		// 	char newChar = (char)index; //changes the index of ASCII to a character
+		// 	String toBeAdded = "" + newChar; //changes the character to a string
+		// 	dictionary.put(index, toBeAdded); //puts string into HashMap with corresponding spot in table
+		// 	sizeOfDictionary++;
+		// }
 		
 		
 		
 		/**
 		 * puts all of the bytes of the file into an array of bytes
 		 */
-		Path path = Paths.get(fileName);
-		System.out.println(path.toAbsolutePath());
-		try {
-			fileContents =  Files.readAllBytes(path);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		// Path path = Paths.get(fileName);
+		// System.out.println(path.toAbsolutePath());
+		// try {
+		// 	fileContents =  Files.readAllBytes(path);
+		// } catch (IOException e) {
+		// 	// TODO Auto-generated catch block
+		// 	e.printStackTrace();
+		// }
 		
 		
 		/**
 		 * turns array of bytes into one long string of 0s and 1s
 		 */
-		StringBuilder strBuilder = new StringBuilder ();
-		for (int i = 0; i < fileContents.length; i ++) {
-			byte b1 = fileContents [i];
-			strBuilder.append(String.format("%8s", Integer.toBinaryString(b1 & 0xFF)).replace(' ', '0'));
-		}
+		// StringBuilder strBuilder = new StringBuilder ();
+		// for (int i = 0; i < fileContents.length; i ++) {
+		// 	byte b1 = fileContents [i];
+		// 	strBuilder.append(String.format("%8s", Integer.toBinaryString(b1 & 0xFF)).replace(' ', '0'));
+		// }
 		
 		
-		System.out.println(decode (strBuilder.toString())); 
+		// System.out.println(decode (strBuilder.toString())); 
 		
 		
 	}
@@ -61,35 +61,46 @@ public class Decoder {
 	 * @param toBeDecoded is the String that was created by the binary file
 	 * @return the decoded message
 	 */
-	public String decode (String toBeDecoded) throws IOException {
+	public static void decode (String inputFile, String outputFile) throws IOException {
 		
 		
 		StringBuilder toRet = new StringBuilder ();
-		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("sampleOutput.txt")));
+		FileInputStream in = new FileInputStream(inputFile);
+		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("output1.txt")));
+		byte[] byteArray = in.readAllBytes();
+
 		
+		// converts byte array into a giant binary String
+		for(int i = 0; i < byteArray.length; i++)
+		{
+			toRet.append(toBinary(byteArray[i]));
+		}
+
+		System.out.println(toRet);
+
 		/**
 		 * parses String into proper size of each byte
 		 */
 		ArrayList<Integer> parsedInts = new ArrayList<Integer>();
-		for (int i = 0; i < toBeDecoded.length()-9; i+=9) {
-			String toBeChanged = toBeDecoded.substring(i,i+9);
-			parsedInts.add(binStringToInteger(toBeChanged));
+		for (int i = 0; i <= toRet.length()-9; i+=9) {
+			parsedInts.add(binStringToInteger(toRet.substring(i,i+9)));
 		}
-		
 
-		String current = "";
-		String next = "";
-		for (int i = 0; i < parsedInts.size()-1; i++) {
-			current = dictionary.get(parsedInts.get(i));
-			next= dictionary.get(parsedInts.get(i+1));
-			String firstOfNext = next.charAt(0) + "";
-			if (!dictionary.containsValue(current + firstOfNext)) {
-				dictionary.put(sizeOfDictionary, current + firstOfNext);
-				sizeOfDictionary++;
-			}
-			toRet.append(current);
-		}
-		toRet.append(next);
+		System.out.println(parsedInts);
+
+		// String current = "";
+		// String next = "";
+		// for (int i = 0; i < parsedInts.size()-1; i++) {
+		// 	current = dictionary.get(parsedInts.get(i));
+		// 	next= dictionary.get(parsedInts.get(i+1));
+		// 	String firstOfNext = next.charAt(0) + "";
+		// 	if (!dictionary.containsValue(current + firstOfNext)) {
+		// 		dictionary.put(sizeOfDictionary, current + firstOfNext);
+		// 		sizeOfDictionary++;
+		// 	}
+		// 	toRet.append(current);
+		// }
+		// toRet.append(next);
 
 		// initial dictionary values
 		HashMap<Integer, String> map = new HashMap<Integer,String>();
@@ -127,32 +138,42 @@ public class Decoder {
 		
 		out.close();
 
-		return toRet.toString();
+		// return toRet.toString();
 		
 	}
 	
 	public static int binStringToInteger (String input){
-		
-		
    
-        	String binaryString = input;
-        	double convertedDouble = 0;
-	        for (int i = 0; i < binaryString.length(); i++) {
+        	int convertedInt = 0;
+	        for (int i = 0; i < 9; i++) {
 	
-	            if (binaryString.charAt(i) == '1') {
-	                int len = binaryString.length() - 1 - i;
-	                convertedDouble += Math.pow(2, len);
+	            if (input.charAt(i) == '1') {
+	                input += (1 << (8 - i));
 	            }
 	        }
-	
-	        int convertedInt = (int) convertedDouble;
         
 		return convertedInt;
+	}
+
+	public static String toBinary(int num)
+	{
+		String current = Integer.toBinaryString(num);
+		StringBuilder str = new StringBuilder();
+		while(current.length()+str.length() < 8)
+		{
+			str.append("0");
+		}
+		if(current.length() > 8)
+		{
+			current = current.substring(current.length() - 8);
+		}
+		str.append(current);
+		return str.toString();
 	}
 	
 	public static void main (String [] args) throws IOException {
 		long startTime = System.nanoTime();
-		Decoder tester = new Decoder ("encodeTest.bin");
+		decode("output1.dat", "output1.txt");
 		long endTime = System.nanoTime();
 		long duration = (endTime - startTime) / 1000000;
 		System.out.println ("" + duration + " ms");
